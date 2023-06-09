6a49c81568b23f88f3c0a29c7e84ce84f557f680
Jagroshan <jagroshansingh615@gmail.com>
2023-06-09 09:50:27 +0530

    updated about content and resume

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,16 +11,28 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div id="about" className="about section" style={{border:'1px solid whitesmoke'}}>
-      
+    <div
+      id="about"
+      className="about section"
+      style={{ border: "1px solid whitesmoke" }}
+    >
       <Stack
         direction="column"
         align="center"
         spacing="4%"
         margin={{ base: 0, md: "0 5rem", lg: "0 9rem" }}
-        border={'0px'}
+        border={"0px"}
       >
-        <Heading marginTop={'63px'} color={'green'} borderY={'2px'} marginBottom={'2%'} p={'0.5%'} backgroundColor={'gray.100'}>About</Heading>
+        <Heading
+          marginTop={"63px"}
+          color={"green"}
+          borderY={"2px"}
+          marginBottom={"2%"}
+          p={"0.5%"}
+          backgroundColor={"gray.100"}
+        >
+          About
+        </Heading>
         <Box>
           <Image
             className="home-img"
@@ -30,40 +42,51 @@ export const About = () => {
             boxSize="250px"
             src="https://avatars.githubusercontent.com/u/112631904?v=4"
             alt="Profile Image"
-            boxShadow={'outline'}
-            _hover={{boxShadow:'dark-lg', transform:"scale(1.05)",transitionDuration:"0.5s"}}
+            boxShadow={"outline"}
+            _hover={{
+              boxShadow: "dark-lg",
+              transform: "scale(1.05)",
+              transitionDuration: "0.5s",
+            }}
           />
           <Heading id="user-detail-name">Jagroshan Singh</Heading>
         </Box>
 
-        <Heading id="user-detail-intro" size={'md'} color={'grey'} fontStyle={'italic'}>
+        <Heading
+          id="user-detail-intro"
+          size={"md"}
+          color={"grey"}
+          fontStyle={"italic"}
+        >
           A passionate and curious web developer, skilled in MERN stack.
           Proficient in both front-end and back-end with 1400+ hours of coding
-          experience. High adaptability and hunger to learn new technologies.
-          Aims to leverage knowledge and build user-friendly web applications
-          while facilitating organizational goals.
+          experience. Possessing high adaptability and eagerness to learn new
+          technologies. Aiming to leverage knowledge and build user-friendly web
+          applications while facilitating organizational goals.
         </Heading>
 
-        <Flex alignItems={"center"} >
-            <ButtonGroup
-              size={{base:'sm',md:'md'}}
-              isAttached
-              variant="outline"
-              colorScheme="green"
+        <Flex alignItems={"center"}>
+          <ButtonGroup
+            size={{ base: "sm", md: "md" }}
+            isAttached
+            variant="outline"
+            colorScheme="green"
+          >
+            <a
+              href="./Jagroshan_Singh_Resume.pdf"
+              onClick={() =>
+                window.open(
+                  "https://drive.google.com/file/d/1oVXIj9zK7Y7aWzadf9mPdF4FxnIxjInT/view?usp=share_link"
+                )
+              }
+              target="_blank"
+              rel="noreferrer"
+              download={"Jagroshan_Singh_Resume"}
             >
-              <a
-                href="./Jagroshan_Singh_Resume.pdf"
-                onClick={() => window.open("https://drive.google.com/file/d/1oVXIj9zK7Y7aWzadf9mPdF4FxnIxjInT/view?usp=share_link")}
-                target="_blank"
-                rel="noreferrer"
-                download={'Jagroshan_Singh_Resume'}
-              >
-                <Button variant={"solid"} >
-                  Resume
-                </Button>
-              </a>
-            </ButtonGroup>
-          </Flex>
+              <Button variant={"solid"}>Resume</Button>
+            </a>
+          </ButtonGroup>
+        </Flex>
       </Stack>
     </div>
   );
